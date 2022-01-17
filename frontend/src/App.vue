b4c108ef563011411bf47d5c09b552eb8d89e0cb
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2022-01-16 18:50:20 -0600

    Starting the repository

---
+++ frontend/src/App.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+.home
+  .body
+    content
+    navigation
+</template>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -moz-osx-font-smoothing grayscale
+  font-size 62.5%
+  color #2c3e50
+.body
+  display grid
+  grid-template-columns 1fr 1fr
+  gap 30px
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import newsService from './services/news.service';
+import Content from './components/Content.vue';
+import Navigation from './components/Navigation.vue';
+export default defineComponent({
+	data() {
+		return {
+			newsList: [] as Array<any>,
+		};
+	},
+	components: {
+		Content,
+		Navigation,
+	},
+	provide() {
+		return {
+			news: this.newsList,
+		};
+	},
+	mounted() {
+		this.getNews();
+	},
+	methods: {
+		getNews() {
+			newsService
+				.get('')
+				.then(response => this.formatFetch(response.data.articles));
+		},
+		formatFetch(news: any) {
+			for (const key in news) {
+				this.newsList.push({ ...news[key], id: key });
+			}
+		},
+	},
+});
+</script>
---
+++ frontend/src/components/Navigation.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+.container
+	div(v-for="newItem in news")
+		h1 {{newItem.title}}
+</template>
+
+// The inject works
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+	inject: ['news'],
+});
+</script>
